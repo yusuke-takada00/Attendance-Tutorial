c2af26fc53c223f7859f11a00c9d1a0eea256a15
yusuke <yu.neiho@gmail.com>
2019-07-04 08:35:11 +0000

    finish full-fledged-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,19 +3,41 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end 
+  
+  def forget(user)
+    user.forget #Userモデル参照
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
+  #セッションと@current_userを破棄します
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
   
+  #一時的セッションにいるユーザーを返します。
+  #それ以外の場合はcookiesに対応するユーザーを返します。
   def current_user
-    if session[:user_id]
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
+  #現在ログイン中のユーザーがいればtrue,そうでなければfalseを返します
   def logged_in?
     !current_user.nil?
   end
-  
 end
